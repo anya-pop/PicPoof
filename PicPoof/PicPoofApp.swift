7a42c97eb526bf28a3acc15fdef32ab47d24b412
Aleksandar Bursac <aleksbrsc@gmail.com>
2024-11-21 21:57:16 -0500

    Initial Commit

---
+++ PicPoof/PicPoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicPoofApp.swift
+//  PicPoof
+//
+//  Created by Aleks Bursac on 2024-11-21.
+//
+
+import SwiftUI
+
+@main
+struct PicPoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PicPoofTests/PicPoofTests.swift
@@ -0,0 +1,17 @@
+//
+//  PicPoofTests.swift
+//  PicPoofTests
+//
+//  Created by Aleks Bursac on 2024-11-21.
+//
+
+import Testing
+@testable import PicPoof
+
+struct PicPoofTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
