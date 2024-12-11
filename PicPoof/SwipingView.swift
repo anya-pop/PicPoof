1d4aa00d76457151f16e7f55a9f504613a9a4096
Aleksandar Bursac <aleksbrsc@gmail.com>
2024-12-11 18:57:57 -0500

    feat(SwipingView): basic photo information on drag and hold

--- PicPoof/SwipingView.swift
+++ PicPoof/SwipingView.swift
@@ -16,6 +16,12 @@ struct SwipingView: View {
     @State private var currentPhotoIndex = 0
     @State private var deletionList: Set<String> = []
     @State private var dragAmount = CGSize.zero
+    @State private var dragging: Bool = false
+    @State private var showOverlay: Bool = false
+    @State private var photoInfo = "Photo info"
+    @State private var blurAmount: CGFloat = 0
+    @State private var photoInfoOpacity = 0.0
+    
     @State private var isCompleted = false
     let date: (year: String?, month: String?)
 
@@ -36,22 +42,66 @@ struct SwipingView: View {
                         .frame(maxHeight: 600)
                         .offset(dragAmount)
                         .rotationEffect(.degrees(Double(dragAmount.width / 10)))
-                        .opacity(1 - min(abs(dragAmount.width) / 300.0, 0.5))
+                        .opacity(1 - min(abs(dragAmount.width) / 600.0, 0.5))
+                        .blur(radius: blurAmount)
                         .overlay(
-                            Rectangle()
-                                .fill(dragAmount.width > 0
-                                      ? Color.green.opacity(Double(abs(dragAmount.width) / 300.0))
-                                      : Color.red.opacity(Double(abs(dragAmount.width) / 300.0)))
-                                .blendMode(.overlay)
-                                .offset(dragAmount)
-                                .rotationEffect(.degrees(Double(dragAmount.width / 10)))
+                            ZStack {
+                                if showOverlay {
+                                    Text(photoInfo)
+                                        .font(Font.custom("Montserrat", size: 18).weight(.semibold))
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .background(Color.black.opacity(0.7))
+                                        .cornerRadius(10)
+                                        .offset(dragAmount)
+                                        .rotationEffect(.degrees(Double(dragAmount.width / 10)))
+                                        .opacity(photoInfoOpacity)
+                                            .onAppear {
+                                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                                    withAnimation(.easeIn(duration: 0.2)) {
+                                                        if dragging {
+                                                            photoInfoOpacity = 1.0
+                                                            blurAmount = 2
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            .onDisappear {
+                                                DispatchQueue.main.async {
+                                                    photoInfoOpacity = 0.0
+                                                    blurAmount = 0
+                                                    dragging = false
+                                                }
+                                            }
+                                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                                        .padding([.leading], 16)
+                                }
+                                Rectangle()
+                                    .fill(dragAmount.width > 0
+                                          ? Color.green.opacity(Double(abs(dragAmount.width) / 300.0))
+                                          : Color.red.opacity(Double(abs(dragAmount.width) / 300.0)))
+                                    .blendMode(.overlay)
+                                    .offset(dragAmount)
+                                    .rotationEffect(.degrees(Double(dragAmount.width / 10)))
+                            }
                         )
                         .gesture(
                             DragGesture()
                                 .onChanged {
+                                    dragging = true
                                     dragAmount = $0.translation
+                                    showOverlay = true
+                                    photoInfo = "Photo taken at \(photos[currentPhotoIndex].creationDate?.formatted() ?? "Unknown date")"
+                                    
                                 }
                                 .onEnded { gesture in
+                                    dragging = false
+                                    withAnimation(.easeIn(duration: 0.2)) {
+                                        photoInfoOpacity = 0.0
+                                        blurAmount = 0
+                                    }
+                                    showOverlay = false
+                                    
                                     let threshold: CGFloat = 100
                                     let isSwipeRight = gesture.translation.width > threshold
                                     let isSwipeLeft = gesture.translation.width < -threshold
@@ -84,16 +134,29 @@ struct SwipingView: View {
                     }
                     .font(Font.custom("Montserrat", size: 30).weight(.bold))
                     .foregroundColor(.black)
-                    .padding()
+                    .padding(.leading)
+                    Text("\(deletionList.count)")
+                      .font(
+                        Font.custom("Geist", size: 22)
+                          .weight(.medium)
+                      )
+                      .foregroundColor(.black)
                     
                     Spacer()
+                   
+                    Text("\(currentPhotoIndex - deletionList.count)")
+                        .font(
+                            Font.custom("Geist", size: 22)
+                                .weight(.medium)
+                        )
+                        .foregroundColor(.black)
                     
                     Button("KEEP") {
                         keepCurrentPhoto()
                     }
                     .font(Font.custom("Montserrat", size: 32).weight(.bold))
                     .foregroundColor(.black)
-                    .padding()
+                    .padding(.trailing)
                 }
             } else {
                 NavigationLink(
